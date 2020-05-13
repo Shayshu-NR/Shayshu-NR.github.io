9306757b5cb54226bcd18c3d4f50bd238ad842a2
Shayshu Nahata <55864616+Shayshu-NR@users.noreply.github.com>
2020-05-12 22:28:40 -0400

    Change jquery lib and added better js script loading

--- scripts.js
+++ scripts.js
@@ -3,8 +3,9 @@ $(document).ready(function(){
         $('#main_banner').slideDown(1000);
     }, 1000);
 
-    $('#portfolio_button').delay(500).slideDown(1000);
-  
+    setTimeout(function(){
+        $('.square_btn').delay(10).slideDown(1000);
+    }, 1500);
 });
 
 
