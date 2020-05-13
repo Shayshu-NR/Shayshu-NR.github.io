0730cc27911b2a22f3883845f41be45a1039ee95
Shayshu Nahata <55864616+Shayshu-NR@users.noreply.github.com>
2020-05-12 22:55:53 -0400

    Finaly fixed animation

--- scripts.js
+++ scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
     setTimeout(function(){
         $('#main_banner').slideDown(1000);
-        $('#portfolio_button').delay(1000).css('opacity', 0).slideDown(3000).animate(
-            { opacity: 1 },
-            { queue: false, duration: 3000 }
-          );
     }, 1000);
+
+    setTimeout(function(){
+        $('.square_btn').css('opacity', 0)
+        .slideDown('slow')
+        .animate(
+          { opacity: 1 },
+          { queue: false, duration: 'slow' }
+        );
+    }, 2000);
 
 
 
